76e78ea6203d1757519dd4ef8ba32a70da6b3020
brucee <dldnrud123@gmail.com>
2021-12-07 02:30:14 +0900

    wookyung, 21-12-07 props 나머지

--- wk/vuedongsan/src/App.vue
+++ wk/vuedongsan/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-<div class="black-bg" v-if="모달창열렸니 == true">
+<!-- <div class="black-bg" v-if="모달창열렸니 == true">
     <div class="white-bg">
-        <h4>상세페이지임</h4>
-        <p>상세페이지 내용임</p>
+        <h4>{{원룸들[누른거].title}}</h4>
+        <img :src="원룸들[누른거].image" class="room-img" />
+        <p>{{원룸들[누른거].content}}</p>
+        <p>{{원룸들[누른거].price}}</p>
+        <Discount></Discount>
         <button @click="모달창열렸니 = false">Close</button>
     </div>
-</div>
+</div> -->
+
+<Modal :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니"/>
+
 <div class="menu">
     <a v-for="menu in 메뉴들" :key="menu">{{ menu }}</a>
 </div>
@@ -35,24 +41,41 @@
     <span>신고수 : {{ 신고수[2] }}</span>
 </div> -->
 
-<div v-for="room in 원룸들" :key="room">
+<!-- 1.컴포넌트 vue 파일생성    
+     2. import 
+     3. components 에 등록 
+     4. 컴포넌트 등록 명으로 태그 등록 -->
+<Discount/>
+<!-- <Discount v-bind="오브젝트" :이름 ="오브젝트.name" :나이 ="오브젝트.age"/> -->
+
+<!-- 반복 시 이득, but 많이 쓰지않는거 추천 -->
+<!-- <div v-for="(room,i) in 원룸들" :key="room">
     <img :src="room.image" class="room-img" />
-    <h4>{{ room.title }}</h4>
+    <h4 @click="모달창열렸니 = true; 누른거 = i;">{{ room.title }}</h4>
     <p>{{room.price}}원</p>
-</div>
+</div> -->
+
+<Card :원룸="room" v-for="room in 원룸들" :key="room"/>
+<!-- <Card :원룸="원룸들[i]" v-for="(room,i) in 원룸들" :key="room"/> -->
+
 </template>
 
 <script>
-// import oneroom from './assets/oneroom.js'; import { apple, apple2} from
-// './assets/oneroom.js';
+// import oneroom from './assets/oneroom.js'; 
+// import { apple, apple2} from './assets/oneroom.js';
 
 import data from './assets/oneroom.js';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './Card.vue';
 
 export default {
     name: 'App',
     data() {
         return {
-            원룸들 : data,
+            오브젝트 :{name : 'kim', age : 20},
+            누른거: 0,
+            원룸들: data,
             모달창열렸니: false,
             신고수: [0, 0, 0],
             메뉴들: ['Home', 'Shop', 'About'],
@@ -65,7 +88,11 @@ export default {
             this.신고수[num] += 1;
         }
     },
-    components: {}
+    components: {
+        Discount: Discount,
+        Modal : Modal,
+        Card : Card
+    }
 }
 </script>
 
@@ -76,6 +103,13 @@ body {
 
 div {
     box-sizing: border-box;
+}
+
+.discount {
+    background: #eee;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
 }
 
 .black-bg {
---
+++ wk/vuedongsan/src/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+        <h4>{{원룸들[누른거].title}}</h4>
+        <img :src="원룸들[누른거].image" class="room-img" />
+        <p>{{원룸들[누른거].content}}</p>
+        <p>{{원룸들[누른거].price}}</p>
+        <Discount></Discount>
+        <!-- props 재할당 금지 -->
+        <!-- <button @click="모달창열렸니 = false">Close</button> -->
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    props: {
+        원룸들: Array,
+        누른거: Number,
+        모달창열렸니: Boolean
+    },
+    data() {
+        return {
+            // 상위 컴포넌트에서 데이터쓸 경우 최상위 컴포넌트에 선언하는게 좋음
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
