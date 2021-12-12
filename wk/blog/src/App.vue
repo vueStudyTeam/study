31edfc620b6f9fa180d4cfebbc7bd382b91c40eb
brucee <dldnrud123@gmail.com>
2021-12-13 02:49:02 +0900

    wookyung, 21-12-13 blog project upload

---
+++ wk/blog/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class = "container mt-4">
+  <h5>Brucee 의 블로그</h5>
+<p> - Making By Vue - </p>
+</div>
+<List :블로그글 ="블로그글"/>
+</template>
+
+<script>
+import List from './components/List.vue';
+import blog from './assets/blog.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      블로그글 : blog
+    }
+  },
+  components: {
+    List: List
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ wk/blog/src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+ <div v-for="post in 블로그글" :key="post">
+    <h5>{{post.title}}</h5>
+    <p>{{post.date}}</p>
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+props :{
+    블로그글 : Array
+}
+}
+</script>
+
+<style>
+
+</style>
