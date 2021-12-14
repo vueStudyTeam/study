f9dfbde4a206cf476709f017780f540052828c65
hyungtaek.ryu <bedmind10@gmail.com>
2021-12-14 21:54:53 +0900

    20211214 hyungtaek ~vuedongsan

--- ht/codingapple/vuedongsan/src/App.vue
+++ ht/codingapple/vuedongsan/src/App.vue
@@ -1,93 +1,123 @@
 <template>
-<div v-if="1==2">
-  안녕하세요.
-</div>
-<div v-else-if="1==3">안녕하세요 2</div>
-<div v-else>안녕하세요3</div> 
-
-
+<div>
+<Modal :products="products" :modalYn="modalYn" :modalDataIdx="modalDataIdx" v-on:closeModal="closeModal"/>
 <div class="menu">
   <a v-for="(items,i) in menus" :key="i">{{items}}</a>
 </div>
+<Discount v-if="showDiscount == true"/>
 
- <div v-for="(items,i) in products" :key="i">
-  <img :src="products[i].image">
-  <h4>{{products[i].title}}</h4>
-  <p>{{products[i].price}}</p>
-  <button @click="clickModalBtn(i)">자세히</button>
+<button v-on:click="priceSort()">가격순정렬</button>
+<button v-on:click="stringSort()">가나다순정렬</button>
+<button v-on:click="sortBack()">되돌리기</button>
+<Card :item="items" v-for="(items, i) in products" :key="i" v-on:clickModalBtn="clickModalBtn(i)"/>
 </div>
-
-<Discount/>
-<Modal :products="products" :modalYn="modalYn" :modalDataIdx="modalDataIdx"/>
 </template>
 
 <script>
 import products from "./assets/data.js"
 import Discount from "./Discount.vue"
 import Modal from "./Modal.vue"
+import Card from "./Card.vue"
+
 export default {
   name: 'App',
   components : {
     Discount : Discount,
-    Modal :Modal
+    Modal :Modal,
+    Card : Card
   },
   data(){
     return {
+      productsOrigin : [...products],
       modalDataIdx : 0,
       modalYn:false,
+      showDiscount:true,
       reports : [0,0,0],
       menus : ['Home', 'Shop', 'About'],
       products : products
 
     }
   },
+
+  mounted() {
+    let _this = this
+    setTimeout(function(){
+      _this.showDiscount = false;
+    },2000)
+  },  
+
   methods: {
+    closeModal(){
+      this.modalYn = false
+    },
     increase(index){
       this.reports[index] += 1;
     },
     clickModalBtn(index){
-      console.log('test',this.modalYn);
-       this.modalYn = true;
+      this.modalYn = true;
       this.modalDataIdx = index;
-     
-      console.log('test2',this.modalYn)
-      
+    },
+    priceSort(){
+      this.products.sort(function(a,b){
+        return a.price - b.price
+      })
+    },
+    stringSort(){
+      this.products.sort(function(a,b){
+        return a.title < b.title ? -1 : 1
+      })
+    },
+    sortBack(){
+      this.products = [...this.productsOrigin];
     }
   },
 }
 </script>
 
 <style>
-body {
-  margin : 0;
+ body {
+    margin: 0;
 }
 div {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
-
 .discount {
-  background: #eee;
-  padding: 10px;
-  margin: 10px;
-  border-radius: 5px;
+    background: #eee;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
 }
 .black-bg {
-  width: 100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
 }
 .white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
-} 
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
+}
+.room-img {
+    width: 100%;
+    margin-top: 40px;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
 .menu {
-  background : darkslateblue;
-  padding : 15px;
-  border-radius : 5px;
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
 }
 .menu a {
-  color : white;
-  padding : 10px;
+    color: white;
+    padding: 10px;
 }
 </style>
--- ht/codingapple/vuedongsan/src/Modal.vue
+++ ht/codingapple/vuedongsan/src/Modal.vue
@@ -3,7 +3,9 @@
   <div class="white-bg">
     <h4>{{products[modalDataIdx].title}}</h4>
     <p>상세페이지내용임</p>
-    <button>닫기</button>
+    <input v-model="month"/>
+    <p> {{month}} 개월 선택함 : {{products[modalDataIdx].price * month}} 원 </p>
+    <button v-on:click="closeModal">닫기</button>
   </div>
 </div>
  
@@ -12,6 +14,35 @@
 <script>
 export default {
     name:'Modal',
+    methods: {
+      closeModal:function (){
+        this.month = 1;
+        this.$emit('closeModal');
+      }
+    },
+    data() {
+      return {
+          month:1,
+      }
+    },
+    watch :{
+      month(month){
+      console.log('test : ',month,parseInt(month),Number(month),isNaN(parseInt(month)));
+        //한글일 경우 스페이스등 한 음절을 넘어가야 watch가 인식됨. 
+        //회원가입 같은 한글이 들어가는 경우는 watch보다는 인풋 키업등의 기능을 이용하는게 더 좋을듯
+        var exp = /[^0-9]/g;
+        console.log('정규식 :: ',exp.test(Number(month)));
+
+        if(isNaN(Number(month))){
+          alert('숫자만 입력해주세요');
+          this.month = 1;
+        }
+        if(month > 12){
+          alert('너무 큽니다.');
+          this.month = 1;
+        }
+      }
+    },
     props:{
         products : Array,
         modalYn : Boolean,
