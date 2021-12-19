971f3894557c3cab56d51c34254c5da85ffc6789
siwolsmu89 <siwol_smuire89@naver.com>
2021-12-19 19:36:29 +0900

    21-12-19, minsk, before study

---
+++ msk/vuelog/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link to="/list"><p>to List</p></router-link>
+    <router-link to="/"><p>to Main</p></router-link>
+    <router-view :listItems="blogItems"></router-view>
+  </div>
+</template>
+
+<script>
+import blogItems from "./data/blogItems";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      blogItems : blogItems
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ msk/vuelog/src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="item in listItems" :key="item">
+            <h4>{{item.title}}</h4>
+            <p>{{item.content}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: {
+        listItems: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
