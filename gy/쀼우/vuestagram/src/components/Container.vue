89671c1dc5f680f730906ff0b08de3b300a79910
MoonGunyoung <anstnddk@gmail.com>
2021-12-21 23:06:41 +0900

    gy-21-12-21

---
+++ gy/쀼우/vuestagram/src/components/Container.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    
+    <div v-if="step ==0">
+    <Post :게시물="게시물[i]" v-for="(a,i) in 게시물" :key= "i" />
+    </div>
+
+    <div v-if="step ==1">
+    <div class="upload-image" :style="`background-image:url(${이미지})`"></div>
+    <div class="filters">
+    <div class="filter-1"></div>
+    <div class="filter-1"></div>
+    <div class="filter-1"></div>
+    <div class="filter-1"></div>
+    <div class="filter-1"></div>
+  </div>
+  </div>
+
+  <div v-if="step ==2">
+  <div class="upload-image"></div>
+  <div class="write">
+    <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea>
+    <textarea class="write-box">write!</textarea>
+  </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+
+import Post from './Post.vue'
+
+export default {
+    components : {
+        Post,
+    },
+    props : {
+        게시물 : Array,
+        step : Number,
+        이미지 : String,
+    }
+}
+</script>
+
+<style>
+</style>
