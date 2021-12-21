e79bd4eb3f3effccaa1fa0e3c5d77e963072f034
hyungtaek.ryu <bedmind10@gmail.com>
2021-12-21 21:22:04 +0900

    hyungatek 21-12-21 ~vuestagram mitt

--- ht/codingapple/vuedongsan/src/App.vue
+++ ht/codingapple/vuedongsan/src/App.vue
@@ -42,6 +42,11 @@ export default {
   mounted() {
     let _this = this
     setTimeout(function(){
+      //function 에서의 this는 함수를 호출 할 때 어떻게 호출 되었는지에 따라 바인딩할 객체가 동적으로 결정.
+      //메서드 내부함수일 경우에도 this는 전역에 바인딩됨. 
+      //setTimeout은 전역(window)에 등록되어있는 함수
+      //콜백, 함수 내부의 함수는 무조건 window객체를 바라봄 (설계오류..)
+      //화살표 함수는 자체적으론 this가 없다 .. 가장 가까운 객체의 this를 바라봄
       _this.showDiscount = false;
     },2000)
   },  
