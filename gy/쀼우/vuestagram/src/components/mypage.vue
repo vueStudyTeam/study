dc89a25c5ffa02463ec65db17779c2bfef695fae
MoonGunyoung <anstnddk@gmail.com>
2021-12-28 22:17:08 +0900

    gy,21-12-28

---
+++ gy/쀼우/vuestagram/src/components/mypage.vue
@@ -0,0 +1,10 @@
+<template>
+<div style="padding : 10px">
+  <h4>팔로워</h4>
+  <input placeholder="?" />
+  <div class="post-header">
+    <div class="profile"></div>
+    <span class="profile-name">사용자명</span>
+  </div>
+</div>
+</template>
